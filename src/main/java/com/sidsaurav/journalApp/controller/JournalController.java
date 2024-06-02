baad6fceef0509a1f7dd29f206585a753c884e6b
Siddharth Saurav <sid.saurav11@gmail.com>
2024-06-02 14:20:06 +0530

    add delete endpoint

--- src/main/java/com/sidsaurav/journalApp/controller/JournalController.java
+++ src/main/java/com/sidsaurav/journalApp/controller/JournalController.java
@@ -6,28 +6,44 @@ import com.sidsaurav.journalApp.repository.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @RestController
-public class Journal {
+public class JournalController {
 
     @Autowired
     private JournalRepository journalRepo;
 
+    // GET ALL
     @GetMapping("/journals")
     public List<JournalEntry> getAllJournalEntries(){
         return journalRepo.findAll();
     }
 
+    // GET ONE
     @GetMapping("/journal/{id}")
     public JournalEntry getJournalById(@PathVariable String id) {
         return journalRepo.findById(id).orElse(null);
     }
 
+    // POST ONE
     @PostMapping("/journal")
-    public JournalEntry postJournal(@RequestBody JournalEntry req) {
+    public JournalEntry postJournal(@RequestBody JournalEntry req){
+        req.setCreated_at(LocalDateTime.now());
         return journalRepo.save(req);
     }
 
+    // DELETE ONE
+    @DeleteMapping("/journal/{id}")
+    public String deleteJournal(@PathVariable String id) {
+        try{
+            journalRepo.deleteById(id);
+            return "Journal Deleted Successfully";
+        }
+        catch(Exception e){
+            return e.getMessage();
+        }
+    }
 }
--- src/main/java/com/sidsaurav/journalApp/entity/JournalEntry.java
+++ src/main/java/com/sidsaurav/journalApp/entity/JournalEntry.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class JournalEntry {
 
     @Id
-//    @JsonSerialize(using = ToStringSerializer.class)
+    @JsonSerialize(using = ToStringSerializer.class)
     private ObjectId id;
     private String title;
     private String body;
